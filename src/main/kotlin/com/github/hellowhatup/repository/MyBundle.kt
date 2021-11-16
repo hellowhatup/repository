1bfe7d03541a79d3d89cc3065459e0230b28d161
GitHub Action <action@github.com>
2021-11-16 03:50:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/hellowhatup/repository/MyBundle.kt
+++ src/main/kotlin/com/github/hellowhatup/repository/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hellowhatup.repository
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hellowhatup/repository/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hellowhatup/repository/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hellowhatup.repository.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hellowhatup.repository.MyBundle
 
 class MyApplicationService {
 
